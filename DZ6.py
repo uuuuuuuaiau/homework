42206fb5cf3a5b8570b43f7e18217f7ce692d180
Admin <skrinzevskayamasha@gmail.com>
2023-12-17 01:39:58 +0200

    DZ6

---
+++ DZ6.py
@@ -0,0 +1,35 @@
+def divider(a, b):
+
+   try:
+
+       return a / b
+
+   except ZeroDivisionError:
+
+       print("You can't divide by zero!")
+
+       return 0
+
+   except TypeError:
+
+       print("You cannot divide non-numeric values!")
+
+       return 0
+
+   except ValueError:
+
+       print("You cannot divide non-numeric values!")
+
+       return 0
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, 8: 4}
+
+result = []
+
+for key in data:
+
+   res = divider(key, data[key])
+
+   result.append(res)
+
+print(result)
